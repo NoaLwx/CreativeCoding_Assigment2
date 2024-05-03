edffc8a86618e35f5c35bbe6a4f73d3e486b3b1c
NoaLwx <130018250+NoaLwx@users.noreply.github.com>
2024-05-04 00:47:40 +1000

    adding audio???

--- public/script.js
+++ public/script.js
@@ -1,66 +1,166 @@
 document.body.style.margin   = 0
 document.body.style.overflow = `hidden`
 
+// calling the canvas from index file using Id name
 const cnv = document.getElementById (`cnv_element`);
+
+// setting the size of the canvas
 cnv.width = 800;
 cnv.height = (cnv.width * 9) / 16;
 
-
+// 
 const ctx = cnv.getContext(`2d`);
 
+// calling the pixelsorter class in
 const sorter = new PixelSorter(cnv,ctx);
 
-  const img = new Image();
-  img.onload = () => {
-    cnv.height = cnv.width * (img.height / img.width);
-    ctx.drawImage(img, 0, 0, cnv.width, cnv.height);
-    window.imageData = ctx.getImageData (0, 0, cnv.width, cnv.height);
-    sorter.init();
+// 
+const img = new Image();
+
+// 
+img.onload = () => {
+
+  // 
+  cnv.height = cnv.width * (img.height / img.width);
+
+  // 
+  ctx.drawImage(img, 0, 0, cnv.width, cnv.height);
+
+  // 
+  window.imageData = ctx.getImageData (0, 0, cnv.width, cnv.height);
+
+  // 
+  sorter.init();
   };
 
-
+// 
 img.src = `me.jpg`;
 
-// function randNumber (min,max){
-//   return Math.random() * (max - min +1) + min;
-// }
+const audio_context = new AudioContext ()
 
-// function sortPixels(e) {
-//   const pixels = imageData.data;
-  
-//   let areaX = e.clientX; // Current mouse X position
-//   let areaY = e.clientY; // Current mouse Y position
-//   let areaWidth = randNumber (20,50); // Width of the area
-//   let areaHeight = randNumber (100,200); // Height of the area
-   
-//   // Calculate the start and end indices for x and y
-//   let startX = areaX;
-//   let startY = areaY - 200;
-//   let endX = areaX + areaWidth;
-//   let endY = areaY + areaHeight;
+// I put this in as a test, not planning to use all of this code.
+// array of notes for the sounds
+const notes = [ 62, 66, 69, 73, 74, 73, 69, 66 ]
 
-//   for (let y = startY; y < endY; y++){
-//     for (let x = startX ; x < endX; x++){
-//      const index = (y * cnv.width + x) * 4;
-//      const brightness = pixels[index] + pixels[index + 1] + pixels[index + 2];
+// declaring a mutable iterator
+let i = 0
 
-// // Get the brightness of the pixel below
-//  if (y + 1 < endY) {
-//   const indexBelow = ((y + 1) * cnv.width + x) * 4 ;
-//   const brightnessBelow = pixels[indexBelow] + pixels[indexBelow + 1] + pixels[indexBelow + 2];
+// declaring a mutable state value
+let running = false
 
-//   if (brightness < brightnessBelow) {
-//       for (let i = 0; i < 3; i++) {
-//       const temp = pixels[index + i];
-//       pixels[index + i] = pixels[indexBelow + i];
-//       pixels[indexBelow + i] = temp;
-//         }
-//       }
-//     }
-//   }
-// }
-//  ctx.putImageData(imageData, 0, 0);
-// }
+// declaring a mutable variable for 
+// the period of time between notes
+let period = 200
+
+// declaring a mutable variable for
+// the length of the note
+let len = 0
+
+function play_note (note, length) {
+
+    // if the audio context is not running, resume it
+    if (audio_context.state != 'running') init_audio ()
+
+    // create an oscillator
+    const osc = audio_context.createOscillator ()
+
+    // make it a triangle wave this time
+    osc.type = 'triangle'
+
+    // set the value using the equation 
+    // for midi note to Hz
+    osc.frequency.value = 440 * 2 ** ((note - 69) / 12)
+
+    // create an amp node
+    const amp = audio_context.createGain ()
+
+    // connect the oscillator 
+    // to the amp
+    // to the audio out
+    osc.connect (amp).connect (audio_context.destination)
+
+    // the .currentTime property of the audio context
+    // contains a time value in seconds
+    const now = audio_context.currentTime
+
+    // make a gain envelope
+    // start at 0
+    amp.gain.setValueAtTime (0, now)
+
+    // take 0.02 seconds to go to 0.4, linearly
+    amp.gain.linearRampToValueAtTime (0.4, now + 0.02)
+
+    // this method does not like going to all the way to 0
+    // so take length seconds to go to 0.0001, exponentially
+    amp.gain.exponentialRampToValueAtTime (0.0001, now + length)
+
+    // start the oscillator now
+    osc.start (now)
+
+    // stop the oscillator 1 second from now
+    osc.stop  (now + length)
+}
+
+// declaring a function that plays the next note
+function next_note () {
+
+    // use the iterator to select a note from 
+    // the notes array and pass it to the 
+    // play_note function along with the 
+    // len variable to specify the length of the note
+    play_note (notes[i], len)
+
+    // iterate the iterator
+    i++
+
+    // if i gets too big
+    // cycle back to 0
+    i %= notes.length
+}
+
+// this is a recursive function
+function note_player () {
+
+    // play the next note
+    next_note ()
+
+    // if running is true
+    // it uses setTimeout to call itself 
+    // after period milliseconds
+    if (running) setTimeout (note_player, period)
+}
+
+// this function handles the mouse event
+// when the cursor enters the canvas
+cnv.onpointerenter = e => {
+
+    // set running to true
+    running = true
+
+    // initiate the recurseive note_player function
+    note_player ()
+}
+
+// this function handles the mouse event
+// when the cursor moves over the canvas
+cnv.onpointermove = e => {
+
+    // as the cursor goes from left to right
+    // len gos from 0 to 5
+    len = 5 * e.offsetX / cnv_0.width
+
+    // as the cursor goes from bottom to top
+    // period goes from 420 to 20 (milliseconds)
+    period = 20 + ((e.offsetY / cnv_0.height) ** 2) * 400
+}
+
+// this function handles the mouse event
+// when the cursor leaves the canvas
+cnv.onpointerleave = e => {
+
+    // set running to false
+    running = false
+}
 
 cnv.addEventListener('mousemove', (e) => {
   sorter.sortPixels(e);
