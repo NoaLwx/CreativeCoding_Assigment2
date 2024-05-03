0330855f27365c257bd7b37c5512e6a5701c71cd
NoaLwx <130018250+NoaLwx@users.noreply.github.com>
2024-05-03 23:21:46 +1000

    test

--- public/script.js
+++ public/script.js
@@ -2,21 +2,62 @@ document.body.style.margin   = 0
 document.body.style.overflow = `hidden`
 
 const cnv = document.getElementById (`cnv_element`)
-cnv.width = innerWidth
-cnv.height = innerHeight
+cnv.width = cnv.parentNode.scrollWidth;
+  cnv.height = (cnv.width * 9) / 16;
 
-const ctx = cnv.getContext (`2d`)
+  const ctx = cnv.getContext(`2d`);
 
-const draw_frame = () => {
-   ctx.fillStyle = `turquoise`
-   ctx.fillRect (0, 0, innerWidth, innerHeight)
+  const img = new Image();
+  img.onload = () => {
+    cnv.height = cnv.width * (img.height / img.width);
+    ctx.drawImage(img, 0, 0, cnv.width, cnv.height);
+    window.imageData = ctx.getImageData (0, 0, cnv.width, cnv.height);
+    // sortPixels();
+  };
 
-   requestAnimationFrame (draw_frame)
+
+img.src = `/w5/catnada.png`;
+
+function randNumber (min,max){
+  return Math.random() * (max - min +1) + min;
 }
 
-draw_frame ()
+function sortPixels(e) {
+  const pixels = imageData.data;
+  
+  let areaX = e.clientX; // Current mouse X position
+  let areaY = e.clientY; // Current mouse Y position
+  let areaWidth = randNumber (20,50); // Width of the area
+  let areaHeight = randNumber (100,200); // Height of the area
+   
+  // Calculate the start and end indices for x and y
+  let startX = areaX;
+  let startY = areaY - 200;
+  let endX = areaX + areaWidth;
+  let endY = areaY + areaHeight;
 
-window.onresize = () => {
-   cnv.width = innerWidth
-   cnv.height = innerHeight   
+  for (let y = startY; y < endY; y++){
+    for (let x = startX ; x < endX; x++){
+     const index = (y * cnv.width + x) * 4;
+     const brightness = pixels[index] + pixels[index + 1] + pixels[index + 2];
+
+// Get the brightness of the pixel below
+ if (y + 1 < endY) {
+  const indexBelow = ((y + 1) * cnv.width + x) * 4 ;
+  const brightnessBelow = pixels[indexBelow] + pixels[indexBelow + 1] + pixels[indexBelow + 2];
+
+  if (brightness < brightnessBelow) {
+      for (let i = 0; i < 3; i++) {
+      const temp = pixels[index + i];
+      pixels[index + i] = pixels[indexBelow + i];
+      pixels[indexBelow + i] = temp;
+        }
+      }
+    }
+  }
 }
+ ctx.putImageData(imageData, 0, 0);
+}
+
+cnv.addEventListener('mousemove', sortPixels);
+
